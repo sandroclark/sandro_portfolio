1cbb95962c2322020bcb76f0d0e210569e82989b
sandroclark <sandro93@g.ucla>
2020-05-27 13:11:55 -0700

    Fixed Resume and small bugs

--- src/components/projects.js
+++ src/components/projects.js
@@ -63,15 +63,76 @@ class Projects extends Component {
       )
     } else if(this.state.activeTab === 1) {
       return (
-        <div><h1>This is Data Science(under construction)</h1></div>
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://i1.wp.com/canopylab.com/wp-content/uploads/2019/11/shutterstock_1455391502-2.jpg?ssl=1) center / cover' }} >Venezuelan Interference</CardTitle>
+            <CardText>
+              The negative consequences of adversarial machine learning has become more apparent with the recent interference into the U.S election by Russia. This project utilizes Twitter's Dataset to examine the role and scope of Venezuelan interference in the '16 and '18 elections.
+            </CardText>
+            <CardActions border>
+              <a href="https://github.com/sandroclark/Venezuelan-Intrusion"><Button colored>GitHub</Button></a>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       )
     } else if(this.state.activeTab === 2) {
       return (
-        <div><h1>This is NLP(under construction)</h1></div>
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://i1.wp.com/canopylab.com/wp-content/uploads/2019/11/shutterstock_1455391502-2.jpg?ssl=1) center / cover' }} >Venezuelan Interference</CardTitle>
+            <CardText>
+              The negative consequences of adversarial machine learning has become more apparent with the recent interference into the U.S election by Russia. This project utilizes Twitter's Dataset to examine the role and scope of Venezuelan interference in the '16 and '18 elections.
+            </CardText>
+            <CardActions border>
+              <a href="https://github.com/sandroclark/Venezuelan-Intrusion"><Button colored>GitHub</Button></a>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://techcrunch.com/wp-content/uploads/2016/05/robot-customer-service.png?w=1390&crop=1) center / cover' }} >Covid Bot</CardTitle>
+            <CardText>
+              This is my first internet deployed AI: a chatbot. This project  was the bridge for me between front end development and AI.
+            </CardText>
+            <CardActions border>
+              <a href="https://github.com/sandroclark/Quarantine_Bot"><Button colored>GitHub</Button></a>
+              <a href="https://quarantine-bot.herokuapp.com"><Button colored>Live Demo</Button></a>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+
+
+        </div>
       )
     } else if(this.state.activeTab === 3) {
       return (
-        <div><h1>This is AI(under construction)</h1></div>
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://techcrunch.com/wp-content/uploads/2016/05/robot-customer-service.png?w=1390&crop=1) center / cover' }} >Covid Bot</CardTitle>
+            <CardText>
+              This is my first internet deployed AI: a chatbot. This project  was the bridge for me between front end development and AI.
+            </CardText>
+            <CardActions border>
+              <a href="https://github.com/sandroclark/Quarantine_Bot"><Button colored>GitHub</Button></a>
+              <a href="https://quarantine-bot.herokuapp.com"><Button colored>Live Demo</Button></a>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       )
     }
 
@@ -83,7 +144,7 @@ class Projects extends Component {
     return(
       <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          <Tab>Python</Tab>
+          <Tab>Projects</Tab>
           <Tab>Data Science</Tab>
           <Tab>NLP</Tab>
           <Tab>AI</Tab>
--- src/components/resume.js
+++ src/components/resume.js
@@ -36,33 +36,38 @@ class Resume extends Component {
             <hr style={{borderTop: '3px solid #833fb2'}} />
 
 
-
-            <Education
-              startYear={2012}
-              endYear={2014}
-              schoolName="Butte College"
-              schoolDescription="I studied for two years at my local JC where I received a Associates Degree in Social Behavioral Sciences"
-               />
-
-               <Education
-                 startYear={2014}
-                 endYear={2017}
-                 schoolName="University of California Los Angeles"
-                 schoolDescription="While attending this world class institution I studied History and Political Science, receiving my Bachelors Degree in History."
-                  />
-
-
-
             <Education
               startYear={"April 2019"}
               endYear={"August 2019"}
               schoolName="Galvanize: Data Science Immersive"
               schoolDescription="This coding bootcamp exposed me to a variety of Data Science/AI techniques and processes, with my case study being a NLP focused project utilizing sentiment analysis, stylometry, and topic modeling "
             />
+            <Education
+              startYear={2014}
+              endYear={2017}
+              schoolName="University of California Los Angeles"
+              schoolDescription="I studied History and Political Science, receiving my Bachelors Degree in History."
+               />
+
+               <Education
+                 startYear={2012}
+                 endYear={2014}
+                 schoolName="Butte College"
+              schoolDescription="I studied for two years at my local JC where I received a Associates Degree in Social Behavioral Sciences"
+                  />
+
+
+
             <hr style={{ borderTop: '3px solid #833fb2' }} />
 
               <h2>Experience</h2>
 
+            <Experience
+              startYear={2019}
+              endYear={2020}
+              jobName="Data Scientist Internship at Lincode"
+              jobDescription="During this 8 month internship I gained work experience working a Computer Vision startup in Silicon Valley."
+            />
             <Experience
               startYear={2014}
               endYear={2019}
@@ -70,15 +75,10 @@ class Resume extends Component {
               jobDescription="My work experience prior to learning programming was varied. Some relevant work experience included working under a Python developer where I was first exposed to OOP and ML concepts."
               />
 
-              <Experience
-                startYear={2019}
-                endYear={2020}
-                jobName="Data Scientist Internship at Lincode"
-                jobDescription="During this 8 month internship I gained work experience working a Computer Vision startup in Silicon Valley."
-                />
+
               <hr style={{borderTop: '3px solid #833fb2'}} />
               <h2 >Skills</h2>
-      <div className="social-links">
+      <div className="social-links-res">
 
           {/* html5 */}
            <i className="fa fa-html5" aria-hidden="true" />
